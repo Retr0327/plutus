b602d2a1957f0d90467477bc8ff37e09b67a0dc8
Retr0327 <retr0327.dev@gmail.com>
2026-04-04 21:14:54 +0800

    feat: implement invoice line item mapper

---
+++ api/src/infrastructure/mappers/invoice-line-item.mapper.ts
@@ -0,0 +1,47 @@
+import { InvoiceLineItem } from '@plutus/domain/invoice/invoice-line-item/invoice-line-item';
+import { InvoiceLineItem as InvoiceLineItemPO } from '@modules/postgres/entities/invoice-line-item.entity';
+import { AdjustmentMapper } from './adjustment.mapper';
+
+export class InvoiceLineItemMapper {
+  static toDomain(po: InvoiceLineItemPO) {
+    const adjustments = (po.adjustments ?? []).map((adjustment) =>
+      AdjustmentMapper.toDomain(adjustment),
+    );
+
+    return InvoiceLineItem.from({
+      id: po.id,
+      invoiceId: po.invoiceId,
+      campaignLineItemId: po.campaignLineItemId,
+      name: po.name ?? '',
+      actualAmount: Number(po.actualAmount),
+      adjustments,
+    });
+  }
+
+  static toPersistence(item: InvoiceLineItem) {
+    const po: Partial<InvoiceLineItemPO> = {
+      id: item.id.value,
+      invoiceId: item.invoiceId.value,
+      campaignLineItemId: item.campaignLineItemId.value,
+      name: item.name.value,
+      actualAmount: item.actualAmount.value,
+    };
+    return po;
+  }
+
+  static toDto(item: InvoiceLineItem) {
+    const adjustments = item.adjustments.items.map((adjustment) =>
+      AdjustmentMapper.toDto(adjustment),
+    );
+    return {
+      id: item.id.value,
+      invoiceId: item.invoiceId.value,
+      campaignLineItemId: item.campaignLineItemId.value,
+      name: item.name.value,
+      actualAmount: item.actualAmount.value,
+      adjustmentsTotal: item.adjustmentsTotal.value,
+      billableAmount: item.billableAmount.value,
+      adjustments,
+    };
+  }
+}
